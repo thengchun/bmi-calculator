8b95eeaf6dce063b28bc925476f4f0d9088e3fd8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-04 17:48:33 +0000

    Deploy shan-wei/bmi-calculator to github.com/shan-wei/bmi-calculator.git:gh-pages

---
+++ precache-manifest.6d607c51e76aec4883da20ffc7676946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e22956e8f561628b1822acb3feb523",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "8a774a5d5b60b0658a84",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "8a774a5d5b60b0658a84",
+    "url": "/bmi-calculator/static/js/main.0bc3292d.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
